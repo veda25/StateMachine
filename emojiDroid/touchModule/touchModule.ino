15276c31c1c8afd2409cddd419cd5d7384eb38ab
Tyson Moll (vultures) <tyson.moll@mail.utoronto.ca>
2018-10-23 20:11:20 -0400

    Transferring touch code to seperate file, re-implementing desired led pins

--- emojiDroid/touchModule/touchModule.ino
+++ emojiDroid/touchModule/touchModule.ino
@@ -1,8 +1,3 @@
-#include <Servo.h>
-
-#define SERVO_A 7 // Note: no semicolons in macro definitions
-#define SERVO_B 8 
-
 // Variable Setup 
 int capValue[5];
 int capSense[5];
@@ -10,31 +5,17 @@ int ledPins[5];
 
 void setup() {
 
-  // Servo Setup
-  Servo armServo;
-  Servo headServo;
-  armServo.attach(6); // Pin 6
-  headServo.attach(3); // Pin 3
-  int servoTarget = 0;
-  /* servoTarget Values (for armServo):
-   * 0 = Shoulder Rotator
-   * 1 = Shoulder Raise
-   * 2 = Elbow
-   * etc: undefined
-   * 
-   */
-
-  // Pin references
+  // Pin designation
   capSense[0] = 8;
   capSense[1] = 9;
   capSense[2] = 10;
   capSense[3] = 11;
   capSense[4] = 12;
-  ledPins[0] = 5;
-  ledPins[1] = 9;
-  ledPins[2] = 10;
-  ledPins[3] = 11;
-  ledPins[4] = 13;
+  ledPins[0] = 2;
+  ledPins[1] = 3;
+  ledPins[2] = 4;
+  ledPins[3] = 5;
+  ledPins[4] = 6;
 
   // Set up pin I/O
   for (int i=0;i<5;i++) { 
@@ -42,7 +23,7 @@ void setup() {
     pinMode(ledPins[i], OUTPUT);
   }
 
-  // Default Serial Value (9600)
+  // Initialize Serial 
   Serial.begin(9600);
 }
 
